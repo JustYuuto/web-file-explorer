e52fa1cbbc6c0b464bd3684e79a994c423d7ba7c
NetherMC <thelio.trinite@outlook.com>
2023-02-21 17:37:33 +0100

    eslint (indent)

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     '@typescript-eslint'
   ],
   'rules': {
-    'indent': ['error', 2],
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always']
   }
